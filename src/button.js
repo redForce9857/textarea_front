373622fa30aba51b7aeda2924a75749e3e21f8d8
redForce9857 <redforce9857@gmail.com>
2022-11-07 13:00:00 +0600

    fix/button/Ricroll/cleanCode

--- src/button.js
+++ src/button.js
@@ -1,12 +1,8 @@
-
 import React from "react";
 import "./index.css";
-import {useNavigate} from 'react-router-dom'
-
 
 
 function RickRoll(){
-	const navigate = useNavigate()
 	const nav = (e) => {
 		window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1';
 	}
